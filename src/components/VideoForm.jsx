48b46fc272c7b7fb735ea73491622e7a7ba60e5f
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-08-15 22:15:07 +0200

    Add useForm to form

--- src/components/VideoForm.jsx
+++ src/components/VideoForm.jsx
@@ -1,51 +1,47 @@
-import useStore from '../hooks/useStore';
+import { useForm } from 'react-hook-form';
 
 import StyledForm from './StyledForm';
 import StyledInput from './StyledInput';
 import StyledLable from './StyledLabel';
 
 export default function VideoForm() {
-	const [inputLinkValue, setInputLinkValue] = useStore('');
-	const [inputTitleValue, setInputTitleValue] = useStore('');
-
-	const videoLink = useStore(state => state.videoLink);
-	const setVideoLink = useStore(state => state.setVideoLink);
-	const videoTitle = useStore(state => state.videoTitle);
-	const setVideoTitle = useStore(state => state.setVideoTitle);
+	const { register, handleSubmit } = useForm();
 
 	return (
-		<StyledForm
-			onSubmit={event => {
-				event.preventDefault();
-				setVideoLink(inputLinkValue);
-				setVideoTitle(inputTitleValue);
-				setInputLinkValue('');
-				setInputTitleValue('');
-			}}
-		>
-			<StyledLable htmlFor="link">
-				YouTube link
-				<StyledInput
-					onChange={event => {
-						setInputLinkValue(event.target.value);
-					}}
-					value={videoLink}
-					type="text"
-					id="link"
-				/>
-			</StyledLable>
-			<StyledLable htmlFor="title">
-				Video title
-				<StyledInput
-					onChange={event => {
-						setInputTitleValue(event.target.value);
-					}}
-					type="text"
-					id="title"
-					value={videoTitle}
-				/>
-			</StyledLable>
-			<button>Submit</button>
-		</StyledForm>
+		<>
+			<StyledForm
+				onSubmit={handleSubmit(data => {
+					console.log(data);
+				})}
+			>
+				<StyledLable htmlFor="link">
+					YouTube link
+					<StyledInput
+						{...register('YouTubeLink', {
+							required: 'This field is required',
+							minLength: {
+								value: 15,
+								message: 'Min. length is 15 symbols',
+								pattern: /^(https:\/\/www.youtube.com\/)[^\s$.#]*$/gi,
+							},
+						})}
+						type="text"
+						id="link"
+					/>
+				</StyledLable>
+				<StyledLable htmlFor="title">
+					Video title
+					<StyledInput
+						{...register('videoTitle', {
+							required: 'This field is required',
+							minLength: { value: 15, message: 'Min. length is 15 symbols' },
+						})}
+						type="text"
+						id="title"
+					/>
+				</StyledLable>
+				<button>Submit</button>
+			</StyledForm>
+		</>
 	);
 }
--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,23 +1,14 @@
-import {nanoid} from 'nanoid';
+import { nanoid } from 'nanoid';
 import create from 'zustand';
 
 const useStore = create(set => ({
-	videoLink: [],
-	setVideoLink: link => {
+	video: [],
+	setVideo: (link, title) => {
 		set(state => {
 			return {
-				videoLink: [...state.videoLink, {link, id: nanoid()}],
-			};
-		});
-	},
-	videoTitle: [],
-	setVideoTitle: title => {
-		set(state => {
-			return {
-				videoLink: [...state.videoTitle, {title, id: nanoid()}],
+				video: [...state.video, { link, title, id: nanoid() }],
 			};
 		});
 	},
 }));
-
 export default useStore;
