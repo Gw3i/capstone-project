d5fd1489d6ee04b39a908ab9c2ae9b63f094baf1
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-08-17 10:58:07 +0200

    Write test to render submit button

--- src/components/VideoForm.spec.js
+++ src/components/VideoForm.spec.js
@@ -6,9 +6,30 @@ import '@testing-library/jest-dom';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+import StyledButton from './StyledButton';
 import VideoForm from './VideoForm';
 
 describe('VideoForm', () => {
+	describe('Button component', () => {
+		it('should render children', () => {
+			const text = 'Submit';
+			render(<StyledButton>{text}</StyledButton>);
+			const button = screen.getByText(text);
+			expect(button).toBeInTheDocument();
+		});
+	});
+
+	describe('Button component', () => {
+		it('should allow clicks', async () => {
+			const handleClick = jest.fn();
+			const text = 'Submit';
+			render(<StyledButton onClick={handleClick}>{text}</StyledButton>);
+			const button = screen.getByText(text);
+			await userEvent.click(button);
+			expect(handleClick).toHaveBeenCalledTimes(1);
+		});
+	});
+
 	it('should show error message when input is empty', async () => {
 		const user = userEvent.setup();
 		render(<VideoForm />);
