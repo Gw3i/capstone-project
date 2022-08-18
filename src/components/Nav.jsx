d9cc3f9cf4782e3cc78eb1c562af17cdcd49c8ad
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-08-19 00:00:06 +0200

    Add better semantic structure to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,30 +2,39 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import Icon from './Icon';
-import StyledNav from './StyledNav';
+import StyledList from './StyledList';
+import StyledListItem from './StyledListItem';
 import StyledNavLink from './StyledNavLink';
 
 export default function Nav() {
 	const router = useRouter();
 
 	return (
-		<StyledNav>
-			<Link href="/">
-				<StyledNavLink>
-					<Icon size="24" variant={router.pathname === '/' ? 'homeFilled' : 'home'} />
-					Home
-				</StyledNavLink>
-			</Link>
-
-			<Link href="/create">
-				<StyledNavLink>
-					<Icon
-						size="24"
-						variant={router.pathname === '/create' ? 'createFilled' : 'create'}
-					/>
-					Home
-				</StyledNavLink>
-			</Link>
-		</StyledNav>
+		<nav>
+			<StyledList>
+				<StyledListItem role="listitem">
+					<Link href="/">
+						<StyledNavLink>
+							<Icon
+								size="32"
+								variant={router.pathname === '/' ? 'homeFilled' : 'home'}
+							/>
+							Home
+						</StyledNavLink>
+					</Link>
+				</StyledListItem>
+				<StyledListItem role="listitem">
+					<Link href="/create">
+						<StyledNavLink>
+							<Icon
+								size="32"
+								variant={router.pathname === '/create' ? 'createFilled' : 'create'}
+							/>
+							Create
+						</StyledNavLink>
+					</Link>
+				</StyledListItem>
+			</StyledList>
+		</nav>
 	);
 }
