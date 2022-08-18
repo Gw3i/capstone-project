e3eb8b0e251174ddceb7cd0f436332aa1744b8b8
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-08-18 11:14:31 +0200

    Add responsive iFrame styling

--- src/components/StyledVideoFrame.js
+++ src/components/StyledVideoFrame.js
@@ -2,8 +2,12 @@ import styled from 'styled-components';
 
 const StyledVideoFrame = styled.iframe`
 	width: 100vw;
-	max-width: 800px;
 	height: 100vh;
+
+	@media screen and (min-width: 520px) {
+		max-width: 500px;
+		max-height: 800px;
+	}
 `;
 
 export default StyledVideoFrame;
