6da8d53ba0ddb69ca1050bfd5d56ced5e1756e9e
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-08-19 17:28:30 +0200

    Fix typo

--- src/pages/edit/[id].js
+++ src/pages/edit/[id].js
@@ -4,7 +4,7 @@ import Layout from '../../components/Layout';
 export default function Edit() {
 	return (
 		<Layout>
-			<h1>Edit the video title</h1>
+			<h1>Edit the video title and url</h1>
 			<EditForm />
 		</Layout>
 	);
