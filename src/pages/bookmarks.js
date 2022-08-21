8d97c3ffc7c7690949115700e80c1e49aa6f2e3e
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-08-21 22:16:50 +0200

    Filter videos on bookmarks page to show just bookmarked videos

--- src/pages/bookmarks.js
+++ src/pages/bookmarks.js
@@ -1,9 +1,20 @@
 import Layout from '../components/Layout';
+import VideoCards from '../components/VideoCards/VideoCards';
+import useStore from '../hooks/useStore';
 
 export default function Bookmarks() {
+	const videos = useStore(state => state.videos);
+
 	return (
 		<Layout>
 			<h1>Bookmarks</h1>
+			{videos
+				.filter(video => {
+					return video.isBookmarked === true;
+				})
+				.map(video => {
+					return <VideoCards key={video.id} />;
+				})}
 		</Layout>
 	);
 }
