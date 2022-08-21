36200aa4e8cc45136e8fcc5a24e2c425afd973ed
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-08-21 20:52:20 +0200

    Add more dummy data in store

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -7,6 +7,25 @@ const useStore = create(set => ({
 			YouTubeLink: 'https://www.youtube.com/embed/WOkeB4ZTjFM',
 			videoTitle: 'Rick an Morty on Tour',
 			id: nanoid(),
+			isBookmarked: false,
+		},
+		{
+			YouTubeLink: 'https://www.youtube.com/shorts/Wlj3S21cxRA',
+			videoTitle: 'Was wird das James Webb als nächstes finden?',
+			id: nanoid(),
+			isBookmarked: false,
+		},
+		{
+			YouTubeLink: 'https://www.youtube.com/watch?v=M2PYkZ2Y_Ls',
+			videoTitle: 'Die Entstehung des Menschen',
+			id: nanoid(),
+			isBookmarked: false,
+		},
+		{
+			YouTubeLink: 'https://www.youtube.com/watch?v=VcVsV9Yn4P4',
+			videoTitle: 'Was is Geoengineering?',
+			id: nanoid(),
+			isBookmarked: false,
 		},
 	],
 	setVideos: data => {
