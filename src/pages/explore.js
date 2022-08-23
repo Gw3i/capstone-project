fb70accc26176407a4b5c9eb0f889db9514c64d9
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-08-23 11:01:58 +0200

    Add categories to explore page

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -1,9 +1,15 @@
 import Layout from '../components/Layout';
+import useStore from '../hooks/useStore';
 
 export default function Explore() {
+	const categories = useStore(state => state.categories);
+
 	return (
 		<Layout>
 			<h1>Explore</h1>
+			{categories.map(category => {
+				return <h2 key={category.id}>{category.name}</h2>;
+			})}
 		</Layout>
 	);
 }
