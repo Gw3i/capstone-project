f683a61453cbca470eb9a3c21a7bed442b8fe43a
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-09-03 18:03:13 +0200

    Create AddVideoModal

---
+++ src/components/AddVideoModal.jsx
@@ -0,0 +1,12 @@
+import StyledButton from '../StyledButton';
+import StyledModalSection from '../StyledModalSection';
+
+export default function DeleteModal() {
+	return (
+		<StyledModalSection>
+			<h2>Please add your video to a category</h2>
+			<StyledButton type="button">Cancel</StyledButton>
+			<StyledButton type="button">Add</StyledButton>
+		</StyledModalSection>
+	);
+}
