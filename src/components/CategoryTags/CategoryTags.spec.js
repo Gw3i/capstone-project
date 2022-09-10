ea4da68407eaf8d2d05f03a15e2c6452d9d604e6
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-09-10 14:17:13 +0200

    Add test to Category Tags

--- src/components/CategoryTags/CategoryTags.spec.js
+++ src/components/CategoryTags/CategoryTags.spec.js
@@ -3,20 +3,13 @@
  */
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import { nanoid } from 'nanoid';
 
 import CategoryTags from './CategoryTags';
 
 describe('Category Tags', () => {
 	it('should render the category tags', () => {
-		[
-			{ id: nanoid(), name: 'Cooking' },
-			{ id: nanoid(), name: 'Geography' },
-			{ id: nanoid(), name: 'Math' },
-		];
-
 		render(<CategoryTags />);
-		const link = screen.getByRole('link');
-		expect(link.childElementCount).toBe(3);
+		const listItems = screen.getAllByRole('listitem');
+		expect(listItems).toHaveLength(15);
 	});
 });
