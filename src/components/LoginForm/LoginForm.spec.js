f865ebd3236b1fb722a7495801d2c6da7432ba74
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-09-10 17:29:45 +0200

    Add more test for LoginForm

--- src/components/LoginForm/LoginForm.spec.js
+++ src/components/LoginForm/LoginForm.spec.js
@@ -3,11 +3,24 @@
  */
 
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import LoginForm from './LoginForm';
 
+jest.mock('users', () => ({
+	__esModule: true,
+
+	default: {
+		get: () => ({
+			user: { id: 1, username: 'parent123' },
+		}),
+	},
+}));
+
 describe('Login Form', () => {
+	const user = userEvent.setup();
+
 	it('should render username input', () => {
 		render(<LoginForm />);
 		const userInput = screen.getByPlaceholderText(/parent123/i);
@@ -25,5 +38,30 @@ describe('Login Form', () => {
 		const buttonText = 'Login';
 		const button = screen.getByText(buttonText);
 		expect(button).toBeInTheDocument();
+	});
+
+	it('username input should be empty', () => {
+		render(<LoginForm />);
+		const userInput = screen.getByPlaceholderText(/parent123/i);
+		expect(userInput.value).toBe('');
+	});
+
+	it('username input should be empty', () => {
+		render(<LoginForm />);
+		const userInput = screen.getByPlaceholderText(/\*\*\*\*\*\*\*\*\*\*\*\*\*/i);
+		expect(userInput.value).toBe('');
+	});
+
+	it('should show error message when input is empty', async () => {
+		render(<LoginForm />);
+
+		const errorMessage = screen.queryByText(/This field is required/i);
+		expect(errorMessage).not.toBeInTheDocument();
+
+		const button = screen.getByRole('button', { name: /login/i });
+		await user.click(button);
+
+		const errorMessagesAfterClick = await waitFor(() => screen.getAllByRole('alert'));
+		expect(errorMessagesAfterClick).toHaveLength(2);
 	});
 });
