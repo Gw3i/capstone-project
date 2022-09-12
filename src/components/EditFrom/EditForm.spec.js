330ccaf7827343edd70cb1906a34fd8377999563
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-09-12 09:29:48 +0200

    Fix test for EditForm

--- src/components/EditFrom/EditForm.spec.js
+++ src/components/EditFrom/EditForm.spec.js
@@ -1,52 +1,60 @@
-// /**
-//  * @jest-environment jsdom
-//  */
+/**
+ * @jest-environment jsdom
+ */
 
-// import '@testing-library/jest-dom';
-// import { render, screen, waitFor } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-// import EditForm from './EditForm';
+import EditForm from './EditForm';
 
-// describe('Edit Form', () => {
-// 	const user = userEvent.setup();
+jest.mock('next/router', () => ({
+	useRouter() {
+		return {
+			query: jest.fn(),
+		};
+	},
+}));
 
-// 	it('should render YouTube title input', () => {
-// 		render(<EditForm />);
-// 		const title = screen.getByLabelText(/video title/i);
-// 		expect(title).toBeInTheDocument();
-// 	});
+describe('Edit Form', () => {
+	const user = userEvent.setup();
 
-// 	it('should render category input', () => {
-// 		render(<EditForm />);
-// 		const categoryInput = screen.getByLabelText(/choose a category/i);
-// 		expect(categoryInput).toBeInTheDocument();
-// 	});
+	it('should render YouTube title input', () => {
+		render(<EditForm />);
+		const title = screen.getByLabelText(/video title/i);
+		expect(title).toBeInTheDocument();
+	});
 
-// 	it('should render the Submit button', () => {
-// 		render(<EditForm />);
-// 		const buttonText = 'Submit';
-// 		const button = screen.getByText(buttonText);
-// 		expect(button).toBeInTheDocument();
-// 	});
+	it('should render category input', () => {
+		render(<EditForm />);
+		const categoryInput = screen.getByLabelText(/choose a category/i);
+		expect(categoryInput).toBeInTheDocument();
+	});
 
-// 	it('should render the Cancel button', () => {
-// 		render(<EditForm />);
-// 		const buttonText = 'Cancel';
-// 		const button = screen.getByText(buttonText);
-// 		expect(button).toBeInTheDocument();
-// 	});
+	it('should render the Submit button', () => {
+		render(<EditForm />);
+		const buttonText = 'Submit';
+		const button = screen.getByText(buttonText);
+		expect(button).toBeInTheDocument();
+	});
 
-// 	it('should show error message when input is empty', async () => {
-// 		render(<EditForm />);
+	it('should render the Cancel button', () => {
+		render(<EditForm />);
+		const buttonText = 'Cancel';
+		const button = screen.getByText(buttonText);
+		expect(button).toBeInTheDocument();
+	});
 
-// 		const errorMessage = screen.queryByText(/This field is required/i);
-// 		expect(errorMessage).not.toBeInTheDocument();
+	it('should show error message when input is empty', async () => {
+		render(<EditForm />);
 
-// 		const button = screen.getByRole('button', { name: /submit/i });
-// 		await user.click(button);
+		const errorMessage = screen.queryByText(/This field is required/i);
+		expect(errorMessage).not.toBeInTheDocument();
 
-// 		const errorMessagesAfterClick = await waitFor(() => screen.getAllByRole('alert'));
-// 		expect(errorMessagesAfterClick).toHaveLength(1);
-// 	});
-// });
+		const button = screen.getByRole('button', { name: /submit/i });
+		await user.click(button);
+
+		const errorMessagesAfterClick = await waitFor(() => screen.getAllByRole('alert'));
+		expect(errorMessagesAfterClick).toHaveLength(2);
+	});
+});
